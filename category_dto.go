5f7d583296bf2254116e9a040098a61aa11185d8
wolter <mike.wolter@company.info>
2026-09-14 10:59:18 +0200

    correction to validation

--- category_dto.go
+++ category_dto.go
@@ -11,7 +11,7 @@ import (
 // allowed values when the input is empty or unknown.
 func validateSection(s CategoryType) error {
 	switch canonSection(s) {
-	case "guide", "reference":
+	case "guides", "reference":
 		return nil
 	default:
 		return errors.New("section must be one of [guides reference]")
