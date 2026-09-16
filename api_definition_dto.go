28639c5db5c35f87fd501a3985c6745e2f359436
wolter <mike.wolter@company.info>
2026-09-16 10:26:36 +0200

    refactored validation and multipart handling in APIDefinition client, added required field validations and updated related tests

--- api_definition_dto.go
+++ api_definition_dto.go
@@ -41,6 +41,6 @@ type APIDefinitionParams struct {
 	// Schema is the OpenAPI or Swagger specification as a string (JSON or YAML).
 	FileName     string `json:"filename,omitzero" validate:"required_with=Schema"`
 	Schema       string `json:"schema,omitzero" validate:"required_without=Url UploadSource"`
-	UploadSource string `json:"upload_source,omitzero" validate:"required_with=Url Schema"`
+	UploadSource string `json:"upload_source,omitzero" validate:"required_without=Url Schema"`
 	Url          string `json:"url,omitzero" validate:"required_without=Schema UploadSource"`
 }
